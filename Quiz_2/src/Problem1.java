39577382a5d11914e0b8d91c069759839648312c
dylan <dylan@LAPTOP-GH7L4TR5>
2017-02-26 16:04:51 -0500

    Quiz 2 Question 1

---
+++ Quiz_2/src/Problem1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in); 
+
+		System.out.println("starting tuition: "); 
+		double tuition=sc.nextDouble();
+		System.out.println("percent increase: "); 
+		double rate=sc.nextDouble();
+		double accum = 0;
+		for(int i = 0; i <= 2; i++){
+			accum = accum + tuition;
+			tuition = tuition * (1 + (rate/100));
+		System.out.println(accum);
+		}
+	}
+
+}
